c34d4c2925f82b28e921bdf30238b75b7fcca038
Safa-Thakur19 <safathakur8@gmail.com>
2025-11-30 20:24:56 +0530

    Add files via upload

---
+++ task7,1.c
@@ -0,0 +1,24 @@
+/* Safa Thakur
+57/251P068
+Comps*/ 
+#include <stdio.h>
+int main()
+{
+    int n,i, a[100],large;
+    printf("Enter the number of elements=");
+    scanf("%d",&n);
+    
+    for(i=0; i<=n-1; i++)
+    {
+        printf("Enter a value=");
+        scanf("%d",&a[i]);
+    }
+    large=a[0];
+    for(i=1;i<=n-1;i++)
+    {
+        if(large<a[i])
+           large=a[i];
+    }
+    printf("\nLargest Number is= %d", large);
+    return 0;
+}
